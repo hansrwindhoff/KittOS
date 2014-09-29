d30ffb63b7f33976336e6fe5462aaf97d513b64a
Kittoes <stealthchicken75@gmail.com>
2014-09-29 00:29:50 -0500

    Loader awesomesauce redesign.

---
+++ Web/TypeScript/KittWeb.TypeScript/Core/Loader/loader.js
@@ -0,0 +1,88 @@
+﻿var KittWeb;
+(function (KittWeb) {
+    (function (Core) {
+        var AmdUtilities = (function () {
+            function AmdUtilities() {
+            }
+            AmdUtilities.appendScriptNode = function (node) {
+                var head = document.head || document.getElementsByTagName("head")[0];
+                head.appendChild(node); // append to document header
+            };
+            AmdUtilities.createScriptNode = function (src, addEventsFunc) {
+                var node = document.createElement("script");
+                node.async = true;
+                node.setAttribute("src", src);
+                node.type = "text/javascript";
+
+                addEventsFunc(node); // add events to script node
+
+                return node;
+            };
+            return AmdUtilities;
+        })();
+        Core.AmdUtilities = AmdUtilities;
+
+        // Initialize AmdUtilities
+        (function () {
+            if (window.addEventListener) {
+                AmdUtilities.addEvent = function (element, name, func) {
+                    element.addEventListener(name, func, false);
+                };
+
+                AmdUtilities.removeEvent = function (element, name, func) {
+                    element.removeEventListener(name, func, false);
+                };
+            }
+        })();
+
+        var AmdLoader = (function () {
+            function AmdLoader() {
+            }
+            AmdLoader.define = function (d /*ignored*/ , factory) {
+            };
+
+            AmdLoader.importScript = function (src, id, successFunc, failureFunc) {
+                var defaultFailureFunc = function (event) {
+                    console.log("errored");
+                };
+                var defaultSuccessFunc = function (event) {
+                    console.log("loaded");
+                };
+                var eventsFunc = function (node) {
+                    // Error event handling
+                    AmdUtilities.addEvent(node, "error", function (event) {
+                        defaultFailureFunc(event);
+
+                        if (failureFunc) {
+                            failureFunc(event);
+                        }
+                    });
+
+                    // Load event handling
+                    AmdUtilities.addEvent(node, "load", function (event) {
+                        defaultSuccessFunc(event);
+
+                        if (successFunc) {
+                            successFunc(event);
+                        }
+                    });
+                };
+
+                var node = AmdUtilities.createScriptNode(src, eventsFunc);
+
+                AmdUtilities.appendScriptNode(node);
+            };
+            AmdLoader.m_modules = {};
+            return AmdLoader;
+        })();
+        Core.AmdLoader = AmdLoader;
+    })(KittWeb.Core || (KittWeb.Core = {}));
+    var Core = KittWeb.Core;
+})(KittWeb || (KittWeb = {}));
+
+(function (global, undefined) {
+    global["define"] = KittWeb.Core.AmdLoader.define;
+
+    KittWeb.Core.AmdLoader.importScript("../../RequireJs/manager.j", "");
+})(this, undefined);
+//# sourceMappingURL=loader.js.map
---
+++ Web/TypeScript/KittWeb.TypeScript/Core/Loader/loader.ts
@@ -0,0 +1,70 @@
+﻿declare var define;
+
+module KittWeb.Core {
+    export class AmdUtilities {
+        static addEvent: (element: Element, name: string, func: EventListener) => void;
+        static removeEvent: (element: Element, name: string, func: EventListener) => void;
+        static appendScriptNode(node: HTMLScriptElement) {
+            var head: HTMLHeadElement = document.head || document.getElementsByTagName("head")[0];
+            head.appendChild(node); // append to document header
+        }
+        static createScriptNode(src: string, addEventsFunc: (node: HTMLScriptElement) => any): HTMLScriptElement {
+            var node: HTMLScriptElement = document.createElement("script");
+            node.async = true;
+            node.setAttribute("src", src);
+            node.type = "text/javascript";
+
+            addEventsFunc(node); // add events to script node
+
+            return node;
+        }
+    }
+
+    // Initialize AmdUtilities
+    (() => {
+        if (window.addEventListener) {
+            AmdUtilities.addEvent = (element: Element, name: string, func: EventListener) => {
+                element.addEventListener(name, func, false);
+            };
+
+            AmdUtilities.removeEvent = (element: Element, name: string, func: EventListener) => {
+                element.removeEventListener(name, func, false);
+            }
+        }
+    })();
+
+    export class AmdLoader {
+        private static m_modules: { [key: string]: Function; } = {};
+
+        static define(d: any /*ignored*/, factory: Function) { }
+
+        static importScript(src: string, id: string, successFunc?: EventListener, failureFunc?: EventListener) {
+            var defaultFailureFunc = (event) => { console.log("errored"); };
+            var defaultSuccessFunc = (event) => { console.log("loaded"); };
+            var eventsFunc = (node) => {
+                // Error event handling
+                AmdUtilities.addEvent(node, "error", (event) => {
+                    defaultFailureFunc(event);
+
+                    if (failureFunc) { failureFunc(event); }
+                });
+                // Load event handling
+                AmdUtilities.addEvent(node, "load", (event) => {
+                    defaultSuccessFunc(event);
+
+                    if (successFunc) { successFunc(event); }
+                });
+            };
+            
+            var node = AmdUtilities.createScriptNode(src, eventsFunc);
+
+            AmdUtilities.appendScriptNode(node);
+        }
+    }
+}
+
+((global, undefined) => {
+    global["define"] = KittWeb.Core.AmdLoader.define;
+
+    KittWeb.Core.AmdLoader.importScript("../../RequireJs/manager.j", "");
+})(this, undefined);

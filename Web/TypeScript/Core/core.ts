feb1f174c6260ee079948dcdb4bc647333dd3b20
Kittoes <stealthchicken75@gmail.com>
2014-10-20 17:45:54 -0500

    Refactoring.

--- Web/TypeScript/Core/core.ts
+++ Web/TypeScript/Core/core.ts
@@ -1,38 +1,4 @@
-﻿interface Array<T> {
-    flatten<U>(): U[];
-    flatMap<U>(projection: (any) => U): U[];
-    zip<U>(otherArray: any[], combination: (...any) => U): U[];
-}
-
-((context, undefined) => {
-    Array.prototype.flatten = function () {
-        var results = [];
-
-        this.forEach((innerArray: any[]) => {
-            results.push.apply(results, innerArray);
-        });
-
-        return results;
-    };
-    Array.prototype.flatMap = function (projectFunc) {
-        return this
-            .map((o) => { return projectFunc(o); })
-            .flatten();
-    };
-    Array.prototype.zip = function (otherArray, combineFunc) {
-        var counter: number;
-        var length: number;
-        var results = [];
-
-        for (counter = 0, length = Math.min(this.length, otherArray.length); counter < length; counter++) {
-            results.push(combineFunc(this[counter], otherArray[counter]));
-        }
-
-        return results;
-    };
-})(this, undefined);
-
-module ktw {
+﻿module kcl {
     "use strict";
 
     export enum DeferredStatus {
@@ -40,21 +6,21 @@ module ktw {
         Completed = 1,
         Failed = 2,
     }
-    export interface IDeferred {
+    export interface IDeferred<T> {
         handler: number;
         status: DeferredStatus;
-        value: any;
+        value: T;
     }
-    export interface IIterator {
+    export interface IIterator<T> {
         hasNext: boolean;
-        next(): any;
+        next(): T;
     }
-    export interface IAsyncIterator extends IIterator { nextAsync(): IDeferred; }
+    export interface IAsyncIterator<T> extends IIterator<T> { nextAsync(): IDeferred<T>; }
     export interface IMapper<TInput, TResult> { (input: TInput): TResult; }
     export interface IPredicate { (...args: any[]): boolean; }
-    export interface IReducer<TPrevious, TNext, TResult> { (previous: TPrevious, next: TNext): TResult; }
+    export interface IReducer<TInput, TResult> { (previous: TResult, next: TInput): TResult; }
 
-    export class Iterator<T> implements IAsyncIterator {
+    export class Iterator<T> implements IAsyncIterator<T> {
         private m_collection: Array<T> = [];
         private m_errorCallback: Function;
         private m_position: number = 0;
@@ -62,9 +28,9 @@ module ktw {
         get hasNext(): boolean { return this.m_position < this.m_collection.length; }
 
         enumerate(): void { while (this.hasNext) { this.next(); } }
-        enumerateAsync(): IDeferred {
+        enumerateAsync(): IDeferred<any> {
             var breakIntervalMs: number = 15;
-            var loop: IDeferred = Helpers.repeat(() => {
+            var loop: IDeferred<any> = Helpers.repeat(() => {
                 var start: number = +new Date();
                 while (this.hasNext && ((+new Date() - start) < breakIntervalMs)) { this.next(); }
 
@@ -85,7 +51,7 @@ module ktw {
                 return current;
             }
         }
-        nextAsync(): IDeferred { return Helpers.defer(() => { this.next(); }, this.m_errorCallback); }
+        nextAsync(): IDeferred<T> { return Helpers.defer<T>(() => { this.next(); }, this.m_errorCallback); }
 
         constructor(collection: Array<T>, errorCallback?: Function) {
             this.m_collection = collection;
@@ -94,6 +60,97 @@ module ktw {
     }
 
     export class Helpers {
+        static defer<T>(success?: Function, failure?: Function, durationMs: number = 0): IDeferred<T> {
+            var result: IDeferred<T> = {
+                handler: undefined,
+                status: DeferredStatus.Pending,
+                value: undefined
+            };
+
+            result.handler = setTimeout(() => { // set handler
+                try {
+                    result.value = Helpers.nullApply((success || Helpers.noOp)); // fulfill
+                    result.status = DeferredStatus.Completed; // mark as completed
+                } catch (e) {
+                    result.value = Helpers.nullApply((failure || Helpers.noOp)); // reject
+                    result.status = DeferredStatus.Failed; // mark as failed
+                }
+            }, durationMs);
+
+            return result;
+        }
+        static filter<T>(predicate: IPredicate, iterator: IIterator<T>): IIterator<T> {
+            var filtered: Array<T> = [];
+
+            while (iterator.hasNext) {
+                var value = iterator.next();
+
+                if (Helpers.nullApply(predicate, value)) {
+                    filtered.push(value);
+                }
+            }
+
+            return new Iterator(filtered);
+        }
+        static limit(func: Function, maxExecutions: number = 1) {
+            var numExecutions: number = 0;
+
+            return (...args: any[]) => {
+                if (numExecutions < maxExecutions) {
+                    numExecutions++;
+                    return Helpers.nullApply(func, args);
+                }
+            }
+        }
+        static map<TInput, TResult>(mapper: IMapper<TInput, TResult>, iterator: IIterator<TInput>): IIterator<TResult> {
+            var mapped: Array<TResult> = [];
+
+            while (iterator.hasNext) { mapped.push(Helpers.nullApply(mapper, iterator.next())); }
+
+            return new Iterator(mapped);
+        }
+        static noOp(): void { }
+        static nullApply(func: Function, ...args: any[]): any { return func.apply(null, args); }
+        static partial(func: Function, ...args: any[]) {
+            return (...calledArgs: any[]) => {
+                return Helpers.nullApply(func, args.concat(calledArgs));
+            }
+        }
+        static reduce<TInput, TResult>(reducer: IReducer<TInput, TResult>, iterator: IIterator<TInput>, seedValue?: TResult): TResult {
+            var accumulator: TResult = seedValue;
+
+            while (iterator.hasNext) { accumulator = Helpers.nullApply(reducer, accumulator, iterator.next()); }
+
+            return accumulator;
+        }
+        static repeat<T>(success?: Function, failure?: Function, intervalMs?: number, maxExecutions?: number): IDeferred<T> {
+            var numExecutions: number = 0;
+            var result: IDeferred<T> = {
+                handler: undefined,
+                status: DeferredStatus.Pending,
+                value: undefined
+            };
+
+            result.handler = setInterval(() => {
+                if ((numExecutions < maxExecutions) || !maxExecutions) {
+                    try {
+                        numExecutions++;
+                        result.value = Helpers.nullApply((success || Helpers.noOp)); // fulfill
+                    } catch (e) {
+                        clearInterval(result.handler); // clear interval handler
+                        result.value = Helpers.nullApply((failure || Helpers.noOp)); // reject
+                        result.status = DeferredStatus.Failed; // mark as failed
+                    }
+                } else if (numExecutions === maxExecutions) { // max executions reached
+                    clearInterval(result.handler); // clear interval handler
+                    result.status = DeferredStatus.Completed; // mark as completed
+                }
+            }, intervalMs || 15);
+
+            return result;
+        }
+        static wrap<T>(obj: T): Function { return (): T => { return obj; } }
+
         static isArray(obj: Object): boolean { return Helpers.is(JsTypes.jsArray, obj); }
         static isFunction(obj: Object): boolean { return Helpers.is(JsTypes.jsFunction, obj); }
         static isNumber(str: string): boolean;
@@ -110,89 +167,6 @@ module ktw {
         static isObject(obj: Object): boolean { return Helpers.is(JsTypes.jsObject, obj); }
         static isString(obj: Object): boolean { return Helpers.is(JsTypes.jsString, obj); }
         static isUndefined(obj: Object): boolean { return Helpers.is(JsTypes.jsUndefined, obj); }
-
-        static defer(success?: Function, failure?: Function, durationMs: number = 0): IDeferred {
-            var result: IDeferred = {
-                handler: undefined,
-                status: DeferredStatus.Pending,
-                value: undefined
-            };
-
-            result.handler = setTimeout(() => { // set handler
-                try {
-                    result.value = (success || Helpers.noOp).apply(null, null); // fulfill
-                    result.status = DeferredStatus.Completed; // mark as completed
-                } catch (e) {
-                    result.value = (failure || Helpers.noOp).apply(null, [e]); // reject
-                    result.status = DeferredStatus.Failed; // mark as failed
-                }
-            }, durationMs);
-
-            return result;
-        }
-        static limit(func: Function, maxExecutions: number = 1) {
-            var numExecutions: number = 0;
-
-            return (...args: any[]) => {
-                if (numExecutions < maxExecutions) {
-                    numExecutions++;
-                    return func.apply(null, args);
-                }
-            }
-        }
-        static noOp(): void { }
-        static partial(func: Function, ...funcArgs: any[]) {
-            return (...args: any[]) => {
-                return func.apply(null, funcArgs.concat(args));
-            }
-        }
-        static pipeline(...funcs: any[]): Function {
-            return (...funcArgs: any[]) => {
-                var applyArgs: IReducer<IArguments, Function, Array<Function>> = (args: IArguments, func: Function) => {
-                    return [func.apply(null, args)];
-                };
-
-                return funcs.reduce(applyArgs, funcArgs)[0];
-            }
-        }
-        static repeat(success?: Function, failure?: Function, intervalMs?: number, maxExecutions?: number): IDeferred {
-            var numExecutions: number = 0;
-            var result: IDeferred = {
-                handler: undefined,
-                status: DeferredStatus.Pending,
-                value: undefined
-            };
-
-            result.handler = setInterval(() => {
-                if (maxExecutions && numExecutions === maxExecutions) { // max executions reached
-                    clearInterval(result.handler); // clear interval handler
-                    result.status = DeferredStatus.Completed; // mark as completed
-                } else {
-                    try {
-                        numExecutions++;
-                        result.value = (success || Helpers.noOp).apply(null, null); // fulfill
-                    } catch (e) {
-                        clearInterval(result.handler); // clear interval handler
-                        result.value = (failure || Helpers.noOp).apply(null, [e]); // reject
-                        result.status = DeferredStatus.Failed; // mark as failed
-                    }
-                }
-            }, intervalMs || 10);
-
-            return result;
-        }
-        static wrap(obj: Object): Function { return () => { return obj; } }
-
-        static add: IReducer<number, number, number> = (n1, n2) => { return n1 + n2; };
-        static subtract: IReducer<number, number, number> = (n1, n2) => { return n1 - n2; };
-        static multiply: IReducer<number, number, number> = (n1, n2) => { return n1 * n2; };
-        static divide: IReducer<number, number, number> = (n1, n2) => { return n1 / n2; };
-        static remainder: IReducer<number, number, number> = (n1, n2) => { return n1 % n2; };
-        static min: IReducer<number, number, number> = (n1, n2) => { return Math.min(n1, n2); };
-        static max: IReducer<number, number, number> = (n1, n2) => { return Math.max(n1, n2); };
-        static lessThan: IPredicate = (l: any, r: any) => { return l < r; };
-        static greaterThan: IPredicate = (l: any, r: any) => { return l > r; };
-        static equalTo: IPredicate = (l: any, r: any) => { return l === r; };
 
         private static getClass(obj: Object): string {
             // http://perfectionkills.com/instanceof-considered-harmful-or-how-to-write-a-robust-isarray/
